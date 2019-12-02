2e42ce743f30227d5f920f7c5baaf8528f6bc5c6
Carst Tankink <ctankink@bol.com>
2019-12-02 08:21:41 +0100

    AoC-2019 Day one second star

--- src/01-RocketEquation.kt
+++ src/01-RocketEquation.kt
@@ -1,22 +1,41 @@
 import java.io.File
 
-fun calculateFuel(mass: Int): Long = (mass / 3L) - 2L
+fun calculateFuel(mass: Int): Int = (mass / 3) - 2
+
+fun calculateFuelDifferential(mass: Int): Int {
+    val initialFuel = calculateFuel(mass)
+
+    return when {
+        initialFuel <= 0 -> 0
+        else -> initialFuel + calculateFuelDifferential(initialFuel.toInt())
+    }
+}
 
 fun tests() {
-    assert(calculateFuel(12) == 2L)
-    assert(calculateFuel(14) == 2L)
-    assert(calculateFuel(100756) == 33583L)
+    assert(calculateFuel(12) == 2)
+    assert(calculateFuel(14) == 2)
+    assert(calculateFuel(100756) == 33583)
+
+    assert(calculateFuelDifferential(14) == 2) { "Mismatch on 14" }
+    assert(calculateFuelDifferential(1969) == 966) { "Mismatch on 1969" }
+    assert(calculateFuelDifferential(100756) == 50346) { "Mismatch on 100756" }
 }
 
 fun main() {
     tests()
 
-    val result: Long = File("resources/01-input")
+    val inputs = File("resources/01-input")
         .readLines()
         .map(String::toInt)
-        .map { module -> calculateFuel(module) }
+    val result: Long = inputs
+        .map { module -> calculateFuel(module).toLong() }
         .sum()
 
     println("Result: $result")
 
+    val result2: Long = inputs
+        .map { module -> calculateFuelDifferential(module).toLong() }
+        .sum()
+
+    println("Result: $result2")
 }
