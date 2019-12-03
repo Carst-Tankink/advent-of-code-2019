18965ea5dd0b1538a364a88ec96fe579d2a51be8
Carst Tankink <ctankink@bol.com>
2019-12-03 12:10:10 +0100

    AoC-2019 Day three first start

--- src/03-Wires.kt
+++ src/03-Wires.kt
@@ -1,12 +1,21 @@
 import Direction.*
 import Wires.tests
 import java.io.File
+import kotlin.math.abs
 
 enum class Direction {
     UP, DOWN, LEFT, RIGHT
 }
 
-data class Stretch(val direction: Direction, val start: Pair<Int, Int>, val end: Pair<Int, Int>)
+data class Location(val x: Int, val y: Int) {
+    operator fun plus(other: Location): Location =
+        Location(
+            this.x + other.x,
+            this.y + other.y
+        )
+}
+
+data class Stretch(val direction: Direction, val start: Location, val end: Location)
 
 data class Wire(val stretches: List<Stretch>) {
 
@@ -25,14 +34,14 @@ data class Wire(val stretches: List<Stretch>) {
                         }
 
                         val endPos = when (direction) {
-                            UP -> Pair(0, distance)
-                            DOWN -> Pair(0, -1 * distance)
-                            RIGHT -> Pair(distance, 0)
-                            LEFT -> Pair(-1 * distance, 0)
+                            UP -> Location(0, distance)
+                            DOWN -> Location(0, -1 * distance)
+                            RIGHT -> Location(distance, 0)
+                            LEFT -> Location(-1 * distance, 0)
 
                         }
 
-                        Stretch(direction, Pair(0, 0), endPos)
+                        Stretch(direction, Location(0, 0), endPos)
                     }
             )
     }
@@ -56,17 +65,58 @@ fun main() {
 
     val translated = inputs.map { wire ->
         val translatedStretches: List<Stretch> = wire.stretches.fold(emptyList()) { acc, stretch ->
-            val previousEnd = if (acc.isEmpty()) Pair(0, 0) else acc.last().end
+            val previousEnd = if (acc.isEmpty()) Location(0, 0) else acc.last().end
             acc + Stretch(stretch.direction, stretch.start + previousEnd, stretch.end + previousEnd)
         }
 
         Wire(translatedStretches)
     }
 
+    val wire1 = translated[0]
+    println("Wire1: $wire1")
+    val wire2 = translated[1]
+    println("Wire2: $wire2")
+
+    val isHorizontal: (Stretch) -> Boolean = { stretch -> stretch.direction == LEFT || stretch.direction == RIGHT }
+    val wire1Partitions = wire1.stretches.partition(isHorizontal)
+    println("partitions: $wire1Partitions")
+    val wire2Partitions = wire2.stretches.partition(isHorizontal)
+
+    val intersections: List<Location> = findIntersections(wire1Partitions.first, wire2Partitions.second)
+    val intersections2: List<Location> = findIntersections(wire2Partitions.first, wire1Partitions.second)
+
+    println("Instersections: $intersections")
+    println("Instersections: $intersections2")
+    val shortestDistance = (intersections + intersections2)
+        .filter { l -> l != Location(0, 0) }
+        .map { l -> abs(l.x) + abs(l.y) }
+        .sorted()
+
+    println("Shortest distance to intersection: $shortestDistance")
+
 }
 
-private operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> =
-    Pair(
-        this.first + other.first,
-        this.second + other.second
-    )
+fun findIntersections(horizontals: List<Stretch>, verticals: List<Stretch>): List<Location> {
+
+    val horizontalsAscending = horizontals.map { stretch ->
+        if (stretch.start.x < stretch.end.x) stretch else Stretch(
+            stretch.direction,
+            stretch.end,
+            stretch.start
+        )
+    }
+    val verticalsAscending = verticals.map { stretch ->
+        if (stretch.start.y < stretch.end.y) stretch else Stretch(
+            stretch.direction,
+            stretch.end,
+            stretch.start
+        )
+    }
+
+    return horizontalsAscending.flatMap { h ->
+        verticalsAscending
+            .filter { h.start.x <= it.start.x && it.start.x <= h.end.x }
+            .filter { it.start.y <= h.start.y && h.start.y <= it.end.y }
+            .map { Location(it.start.x, h.start.y) }
+    }
+}
