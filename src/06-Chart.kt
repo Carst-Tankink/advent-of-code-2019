dde90e29856237519a89f39d5ec83878e45face5
Carst Tankink <ctankink@bol.com>
2019-12-06 11:32:14 +0100

    AoC-2019 Day six star 2. Ugly correction but can't be bothered atm

--- src/06-Chart.kt
+++ src/06-Chart.kt
@@ -1,6 +1,6 @@
+import Chart.calculateTransfers
 import Chart.sumOrbits
 import Chart.tests
-import com.sun.tools.corba.se.idl.constExpr.Or
 import java.io.File
 
 data class Orbit(val from: String, val to: String)
@@ -21,14 +21,14 @@ object Chart {
             Orbit("K", "L")
         )
 
-        val sumOrbits = sumOrbits(input)
-        assert(sumOrbits == 42L) { "Was: $sumOrbits"}
-    }
+        val sumOrbits = sumOrbits(input.groupBy { it.from })
+        assert(sumOrbits == 42L) { "Was: $sumOrbits" }
 
-    fun sumOrbits(input: List<Orbit>): Long {
-        val bySource = input.groupBy { it.from }
 
-        return sumOrbitsRec(bySource["COM"].orEmpty(), bySource, 0L)
+        val pathsInput = input + listOf(Orbit("K", "YOU"), Orbit("I", "SAN"))
+        val transfers = calculateTransfers(pathsInput)
+        assert(transfers == 4L) { "Was: $transfers" }
+
     }
 
     private fun sumOrbitsRec(targets: List<Orbit>, bySource: Map<String, List<Orbit>>, currentDistance: Long): Long {
@@ -40,6 +40,45 @@ object Chart {
                 .sum()
         }
     }
+
+    fun sumOrbits(input: Map<String, List<Orbit>>): Long {
+        return sumOrbitsRec(input["COM"].orEmpty(), input, 0L)
+    }
+
+    fun calculateTransfers(map: List<Orbit>): Long {
+        val backtracks = map.groupBy { it.to }
+            .filterValues { v -> v.size == 1 }
+            .mapValues { v -> v.value[0] }
+
+        val backtracksForSanta = findBacktracks("SAN", backtracks, mapOf(Pair("SAN", 0L)))
+        return backtrackToCommon("YOU", backtracks, backtracksForSanta, 0) - 2 // -2 to correct for initial hops YOU and SAN
+    }
+
+    private fun backtrackToCommon(
+        s: String,
+        backtracks: Map<String, Orbit>,
+        backtracksForSanta: Map<String, Long>,
+        i: Long
+    ): Long {
+        return if (backtracksForSanta.containsKey(s)) {
+            backtracksForSanta[s]!! + i
+        } else {
+            val source = backtracks[s]?.from!!
+            backtrackToCommon(source, backtracks, backtracksForSanta, i + 1)
+        }
+    }
+
+    private fun findBacktracks(s: String, backtracks: Map<String, Orbit>, acc: Map<String, Long>): Map<String, Long> {
+        return if (s == "COM") {
+            acc
+        } else {
+            val currentDistance = acc[s]!!
+            val source = backtracks[s]?.from!!
+            findBacktracks(source, backtracks, acc + Pair(source, currentDistance + 1))
+        }
+
+    }
+
 }
 
 fun main() {
@@ -50,6 +89,9 @@ fun main() {
         .map { x -> x.split(')') }
         .map { o -> Orbit(o[0], o[1]) }
 
-    val checkSum = sumOrbits(orbits)
+    val checkSum = sumOrbits(orbits.groupBy { it.from })
     println("Total orbits: $checkSum")
+
+    val transfers = calculateTransfers(orbits)
+    println("Total transfers: $transfers")
 }
