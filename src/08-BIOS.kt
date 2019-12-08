42275cd4bb51baf748c102af2b350676029e11e4
Carst Tankink <ctankink@bol.com>
2019-12-08 13:35:13 +0100

    AoC-2019 Day eight star two, using imperative
    Todo: Functional

--- src/08-BIOS.kt
+++ src/08-BIOS.kt
@@ -5,7 +5,7 @@ fun main() {
     val height = 6
     val layers = File("resources/08-input")
         .readLines()
-        .flatMap { line -> line.chunked(width * height)}
+        .flatMap { line -> line.chunked(width * height) }
         .map { chunk -> chunk.map { c -> Character.getNumericValue(c) } }
 
     val sparsestLayer = layers.minBy { layer -> layer.count { x -> x == 0 } }.orEmpty()
@@ -15,4 +15,19 @@ fun main() {
     val checkSum = ones * twos
 
     println("Checksum $checkSum")
+
+    decode(layers, width, height)
+}
+
+
+fun decode(layers: List<List<Int>>, width: Int, height: Int) {
+    for (y in 0 until height) {
+        for (x in 0 until width) {
+            val index = y * width + x
+            val pixel = layers.find{ l -> l[index] != 2 }!![index]
+            print(if (pixel == 0) ' ' else '*')
+        }
+
+        print('\n')
+    }
 }
