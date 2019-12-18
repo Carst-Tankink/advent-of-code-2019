8e345fbcf463962decb4a538ed28169bb56b1fad
Carst Tankink <ctankink@bol.com>
2019-12-18 14:03:18 +0100

    AoC-2019 Day ten use angles.
    Back to high school geometry for me...

--- src/10-Asteroids.kt
+++ src/10-Asteroids.kt
@@ -1,15 +1,47 @@
 import java.io.File
+import kotlin.math.atan2
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-data class Asteroid(val x: Int, val y: Int)
+data class Asteroid(val x: Double, val y: Double)
+
+fun getVisibileAsteroids(asteroid: Asteroid, allAsteroids: List<Asteroid>): List<Asteroid> {
+    val normalizedByDistance = allAsteroids
+        .map { Asteroid(it.x - asteroid.x, it.y - asteroid.y) }
+        .sortedBy { sqrt(it.x.pow(2) + it.y.pow(2)) }
+
+    return calculateLinesOfSight(asteroid, emptyList(), normalizedByDistance)
+}
+
+tailrec fun calculateLinesOfSight(
+    asteroid: Asteroid,
+    resultSet: List<Asteroid>,
+    othersByDistance: List<Asteroid>
+): List<Asteroid> {
+    return if (othersByDistance.isEmpty()) resultSet else {
+        val closest = othersByDistance[0]
+        val theta = atan2(closest.y, closest.x)
+        val filteredCandidates = (othersByDistance - closest)
+            .filter { !hasAngle(it, theta) }
+        calculateLinesOfSight(asteroid, resultSet + closest, filteredCandidates)
+    }
+}
+
+fun hasAngle(toTest: Asteroid, theta: Double): Boolean {
+    return atan2(toTest.y, toTest.x) == theta
+}
 
 fun main() {
     val asteroids: List<Asteroid> = File("resources/10-input")
         .readLines()
         .mapIndexed { y, line ->
-            line.mapIndexed { x, token -> if (token == '#') Asteroid(x, y) else null }.filterNotNull()
+            line.mapIndexed { x, token -> if (token == '#') Asteroid(x.toDouble(), y.toDouble()) else null }.filterNotNull()
         }
         .flatten()
 
-    println("Asteroids: ${asteroids.size}")
-    println("Asteroid 11: ${asteroids[11]}")
+    val mostVisible = asteroids
+        .map { getVisibileAsteroids(it, asteroids - it) }
+        .maxBy { it.size }
+
+    println("Most visible: ${mostVisible!!.size}")
 }
