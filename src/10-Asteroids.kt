be897fafdd7231ecc1e592396f7a44b674b96f1d
Carst Tankink <ctankink@bol.com>
2019-12-18 14:49:25 +0100

    AoC-2019 Day ten star 2

--- src/10-Asteroids.kt
+++ src/10-Asteroids.kt
@@ -1,16 +1,17 @@
 import java.io.File
+import kotlin.math.PI
 import kotlin.math.atan2
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 data class Asteroid(val x: Double, val y: Double)
 
-fun getVisibileAsteroids(asteroid: Asteroid, allAsteroids: List<Asteroid>): List<Asteroid> {
+fun getVisibileAsteroids(asteroid: Asteroid, allAsteroids: List<Asteroid>): Pair<Asteroid, List<Asteroid>> {
     val normalizedByDistance = allAsteroids
         .map { Asteroid(it.x - asteroid.x, it.y - asteroid.y) }
         .sortedBy { sqrt(it.x.pow(2) + it.y.pow(2)) }
 
-    return calculateLinesOfSight(asteroid, emptyList(), normalizedByDistance)
+    return Pair(asteroid, calculateLinesOfSight(asteroid, emptyList(), normalizedByDistance))
 }
 
 tailrec fun calculateLinesOfSight(
@@ -35,13 +36,53 @@ fun main() {
     val asteroids: List<Asteroid> = File("resources/10-input")
         .readLines()
         .mapIndexed { y, line ->
-            line.mapIndexed { x, token -> if (token == '#') Asteroid(x.toDouble(), y.toDouble()) else null }.filterNotNull()
+            line.mapIndexed { x, token -> if (token == '#') Asteroid(x.toDouble(), y.toDouble()) else null }
+                .filterNotNull()
         }
         .flatten()
 
     val mostVisible = asteroids
         .map { getVisibileAsteroids(it, asteroids - it) }
-        .maxBy { it.size }
+        .maxBy { it.second.size }!!
+    println("Most visible at ${mostVisible.first}: ${mostVisible.second.size}")
 
-    println("Most visible: ${mostVisible!!.size}")
+    val location = mostVisible.first
+    val fromOptimalLocation: Map<Double, List<Asteroid>> = (asteroids - location)
+        .map { Asteroid(it.x - location.x, it.y - location.y) }
+        .sortedBy { sqrt(it.x.pow(2) + it.y.pow(2)) }
+        .groupBy { atan2(it.y, it.x) }
+        .mapKeys { it.key + .5 * PI }
+        .mapKeys { if (it.key < 0) it.key + 2 * PI else it.key }
+        .toSortedMap()
+
+    val order = vaporizeOrder(fromOptimalLocation)
+
+    val target200 = Asteroid(order[199].x + location.x, order[199].y + location.y)
+// !!! Add location back to found target
+    println("First vaporized: $target200")
+
+    println("Magic number: ${target200.x * 100 + target200.y}")
+
+
 }
+
+fun vaporizeOrder(fromOptimalLocation: Map<Double, List<Asteroid>>): List<Asteroid> {
+    fun removeEntries(fromOptimalLocation: Map<Double, List<Asteroid>>): Map<Double, List<Asteroid>> {
+        return fromOptimalLocation.mapValues { it.value.drop(1) }
+            .filterValues { it.isNotEmpty() }
+    }
+
+    tailrec fun passes(acc: List<Asteroid>, asteroidsLeft: Map<Double, List<Asteroid>>): List<Asteroid> {
+        return if (asteroidsLeft.isEmpty()) acc else {
+            val pass: List<Asteroid> =
+                asteroidsLeft.entries.fold(emptyList()) { accc, entry -> accc + entry.value[0] }
+            val left = removeEntries(asteroidsLeft)
+            return passes(acc + pass, left)
+        }
+    }
+
+    return passes(emptyList(), fromOptimalLocation)
+}
+
+
+
