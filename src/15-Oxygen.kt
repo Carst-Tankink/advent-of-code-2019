daa006fc54c2ef01c0edba06c95a1e2bf9b7a489
Carst Tankink <ctankink@bol.com>
2019-12-22 16:48:18 +0100

    AoC-2019 Day 15 star two
    Pretty easy if you remember the walls...

--- src/15-Oxygen.kt
+++ src/15-Oxygen.kt
@@ -37,26 +37,55 @@ data class RepairDroid(
     }
 }
 
-fun searchOxygenTank(robot: RepairDroid): Pair<Location, Long> {
-    tailrec fun rec(robots: List<RepairDroid>, steps: Long, seenLocations: Set<Location>): Pair<Location, Long> {
-        return if (robots.any { it.oxygenPosition != null }) Pair(
-            robots.mapNotNull { it.oxygenPosition }.first(),
-            steps
-        ) else {
-            val newRobots: List<RepairDroid> = robots.flatMap { droid -> Direction.values().map { droid.move(it) } }
-                .filterNot { newRobot -> newRobot.position == robot.position }
-                .filterNot { seenLocations.contains(it.position) }
-
-            rec(newRobots, steps + 1, seenLocations + newRobots.map { robot -> robot.position })
+fun searchOxygenTank(robot: RepairDroid): Triple<Location, Long, Set<Location>> {
+    tailrec fun rec(
+        robots: List<RepairDroid>,
+        steps: Long,
+        oxygenPosition: Location?,
+        seenLocations: Set<Location>
+    ): Triple<Location, Long, Set<Location>> {
+        val newRobots: List<RepairDroid> = robots.flatMap { droid -> Direction.values().map { droid.move(it) } }
+            .filterNot { newRobot -> newRobot.position == robot.position }
+            .filterNot { seenLocations.contains(it.position) }
+        val foundOxygen = oxygenPosition ?: robots.find { it.oxygenPosition != null }?.oxygenPosition
+        return if (newRobots.isEmpty()) {
+            Triple(
+                oxygenPosition!!,
+                steps,
+                seenLocations
+            )
+        } else {
+            val newSteps = if (foundOxygen != null) steps else steps + 1
+            rec(newRobots,
+                newSteps,
+                foundOxygen,
+                seenLocations + newRobots.map { robot -> robot.position })
         }
     }
 
-    return rec(listOf(robot), 0, emptySet())
+    return rec(listOf(robot), 0, null, emptySet())
+}
+
+fun fillWithOxygen(oxygen: Location, map: Set<Location>): Int {
+    tailrec fun rec(done: Set<Location>, minutes: Int): Int {
+        return if (done == map) minutes else {
+            val newOxygen = done
+                .flatMap { location -> Direction.values().map { location.move(it) } }
+                .filter { map.contains(it) }
+                .toSet()
+            rec(done + newOxygen, minutes + 1)
+        }
+    }
+
+    return rec(setOf(oxygen), 0)
 }
 
 fun main() {
     val program = Machine.parseProgram("resources/15-input")
 
-    val (oxygen, steps) = searchOxygenTank(RepairDroid(Machine(program)))
+    val (oxygen, steps, map) = searchOxygenTank(RepairDroid(Machine(program)))
     println("Oxygen is at: $oxygen, taking $steps steps")
+
+    val minutes = fillWithOxygen(oxygen, map)
+    println("Filled in $minutes minutes")
 }
